973a6472d80b10403d61f571f5299f37acd61c1c
Akash <akashbahadursingh5@gmail.com>
2024-06-16 23:52:05 +0530

    update

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ app.use((req,res,next)=>{
     next();
 });
 app.use(express.json());
+app.use(cors());
 app.use(cors({
     origin: "https://connect-taupe-seven.vercel.app",
     credentials: true,
