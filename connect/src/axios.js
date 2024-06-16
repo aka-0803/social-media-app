eedc3248e54ae342762a12b3d4e77e9e7a8dbb1f
Akash <akashbahadursingh5@gmail.com>
2024-06-17 00:32:29 +0530

    updated endpoints

--- connect/src/axios.js
+++ connect/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://social-media-app-server-phi.vercel.app",
+    baseURL: "https://social-media-app-server-phi.vercel.app/api",
     withCredentials: true,
 });
