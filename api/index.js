f76f0a16e8531118cd568ea09d65d05242a11649
Akash <akashbahadursingh5@gmail.com>
2024-06-17 00:43:14 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -10,10 +10,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import multer from "multer";
 
-// app.use((req,res,next)=>{
-//     res.header("Access-Control-Allow-Credentials",true);
-//     next();
-// });
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials",true);
+    next();
+});
 app.use(express.json());
 app.use(cors());
 app.use(cors({
