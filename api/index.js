5cf483e455efab8559c94b0e81f5e204a09b4bf0
Akash <akashbahadursingh5@gmail.com>
2024-06-16 23:11:27 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use((req,res,next)=>{
 });
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://connect-taupe-seven.vercel.app",
 }));
 app.use(cookieParser());
 const storage = multer.diskStorage({
