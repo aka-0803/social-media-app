7271197b80f063f1b65e5ec1cc84826501d9709f
AKASH  BAHADUR SINGH <92441172+aka-0803@users.noreply.github.com>
2024-06-17 00:36:56 +0530

    Update axios.js

--- connect/src/axios.js
+++ connect/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://social-media-app-server-phi.vercel.app/api",
+    baseURL: "https://social-media-app-server-phi.vercel.app",
     withCredentials: true,
 });
