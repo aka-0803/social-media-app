d001738efcc3675f2db074aeedd98aba19e8c4f1
Akash <akashbahadursingh5@gmail.com>
2024-06-16 23:24:34 +0530

    updated

--- connect/src/context/authContext.js
+++ connect/src/context/authContext.js
@@ -8,7 +8,7 @@ export const AuthContextProvider = ({ children }) => {
   );
 
   const login = async(inputs) => {
-    const res = await axios.post("http://localhost:8800/api/auth/login",inputs,{
+    const res = await axios.post("http://social-media-app-server-phi.vercel.app/auth/login",inputs,{
       withCredentials: true,
     });
     setCurrentUser(res.data);
